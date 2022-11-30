a93a306f62c35402b5b914f6e1895d85a54749ed
sahil <HZ332893@HERTZ.NET>
2022-11-30 11:39:47 +0000

    Initial commit

---
+++ storage/storage.go
@@ -0,0 +1,135 @@
+package storage
+
+import (
+	"bytes"
+	"cloud.google.com/go/storage"
+	"context"
+	log "github.com/sirupsen/logrus"
+	"google.golang.org/api/iterator"
+	"io"
+	"os"
+)
+
+func CreateBucket(storageClient *storage.Client, ctx context.Context, projectId string, bucketName string, bktAttrs *storage.BucketAttrs) error {
+	// check if bucket exists
+	var err error = nil
+	_, err = storageClient.Bucket(bucketName).Attrs(ctx)
+	// only create if the bucket does not exist
+	if err == storage.ErrBucketNotExist {
+		log.Infof("Creating bucket")
+		// create the bucket
+		err = storageClient.Bucket(bucketName).Create(ctx, projectId, bktAttrs)
+		if err != nil {
+			log.Fatalf("Error while creating bucket. Error is %v", err)
+			return err
+		}
+		return nil
+	}
+	if err != nil {
+		return err
+	}
+	return err
+}
+
+func ListBuckets(storageClient *storage.Client, ctx context.Context, projectId string) ([]string, error) {
+	// container to hold all bucket names
+	var bucketNames []string
+	var err error = nil
+
+	// list buckets
+	var bktIterator = storageClient.Buckets(ctx, projectId)
+	for {
+		var bucket, err = bktIterator.Next()
+		// Break if at end
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Error while iterating thorugh buckets. Error is %v\n", err)
+			return bucketNames, err
+		}
+
+		bucketNames = append(bucketNames, bucket.Name)
+
+	}
+	return bucketNames, err
+}
+
+func DeleteBucket(storageClient *storage.Client, ctx context.Context, bucketName string) error {
+	// check if bucket exists
+	var err error = nil
+	_, err = storageClient.Bucket(bucketName).Attrs(ctx)
+	// only delete if the bucket exists
+	if err == storage.ErrBucketNotExist {
+		log.Fatalf("Bucket does not exist. Error is %v", err)
+		return err
+	}
+	if err != nil {
+		return err
+	}
+	err = storageClient.Bucket(bucketName).Delete(ctx)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func UploadLocalFile(storageClient *storage.Client, ctx context.Context, bucketName string, localFilePath string, KeyName string) error {
+
+	// check if bucket exists
+	var _, err = storageClient.Bucket(bucketName).Attrs(ctx)
+	if err == storage.ErrBucketNotExist {
+		log.Fatalf("The bucket %s does not exist\n", bucketName)
+		return err
+	}
+	if err != nil {
+		log.Fatalf("Error while loading bucket %s\n", bucketName)
+		return err
+	}
+
+	// create writer object
+	var writer = storageClient.Bucket(bucketName).Object(KeyName).NewWriter(ctx)
+
+	// load local file
+	body, err := os.ReadFile(localFilePath)
+	if err != nil {
+		log.Fatalf("Error while opening file %s. Error is : %v\n", localFilePath, err)
+		return err
+	}
+
+	// write to object
+	if _, err = io.Copy(writer, bytes.NewBuffer(body)); err != nil {
+		log.Fatalf("Error while copying data to object. Error is %v\n", err)
+		return err
+	}
+
+	// Data can continue to be added to the file until the writer is closed.
+	err = writer.Close()
+	if err != nil {
+		log.Fatalf("Error while writing data to file. Error is %v\n", err)
+		return err
+	}
+	return nil
+}
+
+func ReadObject(storageClient *storage.Client, ctx context.Context, bucketName string, objectName string) (error, []byte) {
+	// Read the object from bucket.
+	rc, err := storageClient.Bucket(bucketName).Object(objectName).NewReader(ctx)
+	if err == storage.ErrObjectNotExist {
+		log.Fatalf("Object %s does not exist", objectName)
+		return err, nil
+	}
+	if err != nil {
+		log.Fatal(err)
+		return err, nil
+	}
+	defer rc.Close()
+	body, err := io.ReadAll(rc)
+	if err != nil {
+		log.Fatal(err)
+		return err, nil
+	}
+	return nil, body
+}
+
+
